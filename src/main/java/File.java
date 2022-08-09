25af58cca1c47779baa7db4f7c12df464872dd15
James <1jamesthompson1@gmail.com>
2022-08-09 12:07:25 +1200

    File class testing

---
+++ src/main/java/File.java
@@ -0,0 +1,5 @@
+public class File {
+    public void printOutest() {
+        System.err.println("This is a printout from File.java");
+    }
+}
--- src/main/java/MainGuiClass.java
+++ src/main/java/MainGuiClass.java
@@ -1,5 +1,5 @@
 public class MainGuiClass {
     public static void main(String[] args){
-        System.out.println("Hello World Tester");
+        new File().printOutest();
     }
 }
