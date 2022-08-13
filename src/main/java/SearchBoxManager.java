5f66b2cc4d212c01309524bf8c34fafd841bb08e
Anesu Matanga <matangawebdev@gmail.com>
2022-08-13 14:30:30 +1200

    Added working Search Box with DocumentListener and ActionListener

---
+++ src/main/java/SearchBoxManager.java
@@ -0,0 +1,101 @@
+import javax.swing.*;
+import javax.swing.event.*;
+import javax.swing.text.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.util.regex.*;
+
+public class SearchBoxManager implements DocumentListener, ActionListener {
+    //Variables Initialising
+    protected JTextComponent content;
+    protected Matcher matchWord;
+
+    //Constructor which accepts JTextComponent as the target
+    public SearchBoxManager(JTextComponent component){
+        this.content = component;
+    }
+
+    /**
+     * Implementing DocumentListener which will call startSearching()
+     * method which will start the searching
+     */
+    public void insertUpdate(DocumentEvent event){
+        startSearch(event.getDocument());
+    }
+    public void removeUpdate(DocumentEvent event){
+        startSearch(event.getDocument());
+    }
+
+    @Override
+    public void changedUpdate(DocumentEvent e) {
+
+    }
+
+    public void changeUpdate(DocumentEvent event){
+        startSearch(event.getDocument());
+    }
+
+    /**
+     * startSearch() Method which takes the search text from JTextField
+     *
+     */
+    private void startSearch(Document searchText_doc) {
+        try {
+            String searchText = searchText_doc.getText(0, searchText_doc.getLength());
+
+            //Creating new pattern from the searchText typed
+            Pattern pattern = Pattern.compile(searchText);
+
+            //retrieving the required document and the text
+            Document content_doc = content.getDocument();
+            String body = content_doc.getText(0, content_doc.getLength());
+
+            //creating a Matcher so the Matcher class does the searching
+            matchWord = pattern.matcher(body);
+            keepSearching();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     *keepSearching() method to call find() (on Matcher) to look for
+     * a match
+     */
+    private void keepSearching() {
+        if(matchWord != null){
+            if(matchWord.find() == true) {
+
+                //moveDot and setDot to select the matched word
+                content.getCaret().setDot(matchWord.start());
+                content.getCaret().moveDot(matchWord.end());
+                content.getCaret().setSelectionVisible(true);
+            }
+        }
+    }
+
+    /**
+     * ActionListener Implementation
+     * ActionListener to call keepSearching whenever an action is performed
+     * The actionPerformed will call continueSearch() Method and since the
+     * word has already been matched, the use can continue clicking Enter
+     * to go through the document
+     */
+    public void actionPerformed(ActionEvent event){
+        keepSearching();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
